ff257942048c1c493910f85d0169ad7cf4e1d729
Alex Savenok <sawenok@gmail.com>
2017-09-19 14:14:23 +0300

    fixed coreDomain

--- coffee/sarine.viewer.rough.bundle.coffee
+++ coffee/sarine.viewer.rough.bundle.coffee
@@ -1,5 +1,5 @@
 ###!
-sarine.viewer.rough - v0.0.1 -  Thursday, September 7th, 2017, 6:25:08 PM 
+sarine.viewer.rough - v0.0.1 -  Tuesday, September 19th, 2017, 2:12:38 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 
@@ -59,8 +59,8 @@ class SarineRoughDiamond extends Viewer
 
 		@pluginDimention = if @atomConfig.ImageSize && @atomConfig.ImageSize.height then @atomConfig.ImageSize.height else 300
 
-		@domain = window.stones[0].viewersBaseUrl.replace('content/viewers/', '')
-		@path = @atomConfig.ImagesPath.replace("{stoneId}", window.stones[0].friendlyName)	
+		@domain = window.coreDomain # window.stones[0].viewersBaseUrl.replace('content/viewers/', '')
+		@path = "demo/r2p/" + window.stones[0].friendlyName + "/interactive"	
 
 	convertElement : () ->	
 		@element.append '<div class="threesixty slider360 sarine-slider"><div class="spinner"><span>0%</span></div><ol class="threesixty_images"></ol></div></div>'	 	
@@ -96,8 +96,6 @@ class SarineRoughDiamond extends Viewer
 		_t = @
 		@preloadAssets ()->
 			@firstImageName = _t.atomConfig.ImagePattern.replace("*","1") 
-			#src = "#{configuration.rawdataBaseUrl}/#{_t.atomConfig.ImagesPath}/#{configuration.jewelryId}/slider/#{@firstImageName}#{cacheVersion}"
-
 			src = _t.domain + _t.path + "/" + @firstImageName + cacheVersion
 			
 			_t.loadImage(src).then((img)->	
---
+++ dist/sarine.viewer.rough.bundle.js
@@ -0,0 +1,223 @@
+
+/*!
+sarine.viewer.rough - v0.0.1 -  Tuesday, September 19th, 2017, 2:12:38 PM 
+ The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+ */
+
+(function() {
+  var SarineRoughDiamond, Viewer,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  Viewer = (function() {
+    var error, rm;
+
+    rm = ResourceManager.getInstance();
+
+    function Viewer(options) {
+      console.log("");
+      this.first_init_defer = $.Deferred();
+      this.full_init_defer = $.Deferred();
+      this.src = options.src, this.element = options.element, this.autoPlay = options.autoPlay, this.callbackPic = options.callbackPic;
+      this.id = this.element[0].id;
+      this.element = this.convertElement();
+      Object.getOwnPropertyNames(Viewer.prototype).forEach(function(k) {
+        if (this[k].name === "Error") {
+          return console.error(this.id, k, "Must be implement", this);
+        }
+      }, this);
+      this.element.data("class", this);
+      this.element.on("play", function(e) {
+        return $(e.target).data("class").play.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("stop", function(e) {
+        return $(e.target).data("class").stop.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("cancel", function(e) {
+        return $(e.target).data("class").cancel().apply($(e.target).data("class"), [true]);
+      });
+    }
+
+    error = function() {
+      return console.error(this.id, "must be implement");
+    };
+
+    Viewer.prototype.first_init = Error;
+
+    Viewer.prototype.full_init = Error;
+
+    Viewer.prototype.play = Error;
+
+    Viewer.prototype.stop = Error;
+
+    Viewer.prototype.convertElement = Error;
+
+    Viewer.prototype.cancel = function() {
+      return rm.cancel(this);
+    };
+
+    Viewer.prototype.loadImage = function(src) {
+      return rm.loadImage.apply(this, [src]);
+    };
+
+    Viewer.prototype.setTimeout = function(delay, callback) {
+      return rm.setTimeout.apply(this, [this.delay, callback]);
+    };
+
+    return Viewer;
+
+  })();
+
+  this.Viewer = Viewer;
+
+  SarineRoughDiamond = (function(_super) {
+    __extends(SarineRoughDiamond, _super);
+
+    function SarineRoughDiamond(options) {
+      this.preloadAssets = __bind(this.preloadAssets, this);
+      var css, head, style;
+      SarineRoughDiamond.__super__.constructor.call(this, options);
+      this.isAvailble = true;
+      this.resourcesPrefix = options.baseUrl + "atomic/v1/assets/";
+      this.atomConfig = configuration.experiences.filter(function(exp) {
+        return exp.atom === "roughDiamond";
+      })[0];
+      this.resources = [
+        {
+          element: 'script',
+          src: 'threesixty.min.js'
+        }, {
+          element: 'link',
+          src: 'threesixty.css'
+        }
+      ];
+      css = '.sarine-slider {width: ' + this.atomConfig.ImageSize.width + 'px; height: ' + this.atomConfig.ImageSize.height + 'px}';
+      css += '.spinner {margin-top: 40% !important}';
+      head = document.head || document.getElementsByTagName('head')[0];
+      style = document.createElement('style');
+      style.type = 'text/css';
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      head.appendChild(style);
+      this.pluginDimention = this.atomConfig.ImageSize && this.atomConfig.ImageSize.height ? this.atomConfig.ImageSize.height : 300;
+      this.domain = window.coreDomain;
+      this.path = "demo/r2p/" + window.stones[0].friendlyName + "/interactive";
+    }
+
+    SarineRoughDiamond.prototype.convertElement = function() {
+      return this.element.append('<div class="threesixty slider360 sarine-slider"><div class="spinner"><span>0%</span></div><ol class="threesixty_images"></ol></div></div>');
+    };
+
+    SarineRoughDiamond.prototype.preloadAssets = function(callback) {
+      var element, loaded, resource, totalScripts, triggerCallback, _i, _len, _ref, _results;
+      loaded = 0;
+      totalScripts = this.resources.map(function(elm) {
+        return elm.element === 'script';
+      });
+      triggerCallback = function(callback) {
+        loaded++;
+        if (loaded === totalScripts.length - 1 && callback !== void 0) {
+          return setTimeout((function(_this) {
+            return function() {
+              return callback();
+            };
+          })(this), 500);
+        }
+      };
+      element;
+      _ref = this.resources;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        resource = _ref[_i];
+        element = document.createElement(resource.element);
+        if (resource.element === 'script') {
+          $(document.body).append(element);
+          element.onload = element.onreadystatechange = function() {
+            return triggerCallback(callback);
+          };
+          element.src = this.resourcesPrefix + resource.src + cacheVersion;
+          _results.push(element.type = "text/javascript");
+        } else {
+          element.href = this.resourcesPrefix + resource.src + cacheVersion;
+          element.rel = "stylesheet";
+          element.type = "text/css";
+          _results.push($(document.head).prepend(element));
+        }
+      }
+      return _results;
+    };
+
+    SarineRoughDiamond.prototype.first_init = function() {
+      var defer, _t;
+      defer = $.Deferred();
+      _t = this;
+      this.preloadAssets(function() {
+        var src;
+        this.firstImageName = _t.atomConfig.ImagePattern.replace("*", "1");
+        src = _t.domain + _t.path + "/" + this.firstImageName + cacheVersion;
+        return _t.loadImage(src).then(function(img) {
+          if (img.src.indexOf('data:image') === -1 && img.src.indexOf('no_stone') === -1) {
+            return defer.resolve(_t);
+          } else {
+            _t.isAvailble = false;
+            _t.element.empty();
+            this.canvas = $("<canvas>");
+            this.canvas[0].width = img.width;
+            this.canvas[0].height = img.height;
+            this.ctx = this.canvas[0].getContext('2d');
+            this.ctx.drawImage(img, 0, 0, img.width, img.height);
+            this.canvas.attr({
+              'class': 'no_stone'
+            });
+            _t.element.append(this.canvas);
+            return defer.resolve(_t);
+          }
+        });
+      });
+      return defer;
+    };
+
+    SarineRoughDiamond.prototype.full_init = function() {
+      var defer;
+      defer = $.Deferred();
+      if (this.isAvailble) {
+        this.slider360 = this.element.find('.slider360');
+        this.imagePath = this.domain + this.path + "/";
+        this.filePrefix = this.atomConfig.ImagePattern.replace(/\*.[^/.]+$/, '');
+        this.fileExt = "." + (this.atomConfig.ImagePattern.split('.').pop());
+        this.slider360.ThreeSixty({
+          totalFrames: this.atomConfig.NumberOfImages,
+          endFrame: this.atomConfig.NumberOfImages,
+          currentFrame: 1,
+          imgList: '.threesixty_images',
+          progress: '.spinner',
+          imagePath: this.imagePath,
+          filePrefix: this.filePrefix,
+          ext: this.fileExt + cacheVersion,
+          height: this.pluginDimention,
+          width: this.pluginDimention,
+          navigation: false,
+          responsive: false,
+          onReady: function() {
+            return defer.resolve(this);
+          }
+        });
+      }
+      return defer;
+    };
+
+    SarineRoughDiamond.prototype.play = function() {};
+
+    SarineRoughDiamond.prototype.stop = function() {};
+
+    return SarineRoughDiamond;
+
+  })(Viewer);
+
+  this.SarineRoughDiamond = SarineRoughDiamond;
+
+}).call(this);
