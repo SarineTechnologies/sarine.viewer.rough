3a2a4d0dd9d08ecc0518361b5218d49c1e2cb4cf
ronishoham <roni.shoham@sarine.com>
2018-01-02 17:08:13 +0200

    task 104352

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -2,6 +2,9 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt)
     var target = grunt.option('target') || "";
+    var config = {};
+	config.dist = decideDist();
+
     grunt
     grunt.initConfig({
         config: grunt.file.readJSON(target + "package.json"),
@@ -105,6 +108,14 @@ module.exports = function(grunt) {
             bundle: {
                 dest: target + 'dist/<%= config.name %>.config',
                 src: [target + '<%= config.name %>.config']
+            },
+            dist_root_files: {
+                files: [{
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: config.dist.root,
+                        expand: true
+                    }]
             }
         }
     })
@@ -122,7 +133,8 @@ module.exports = function(grunt) {
         'uglify',
         'clean:postbuild',
         'copyVersion',
-        'copy:bundle'
+        'copy:bundle',
+        'copy:dist_root_files'
     ]);
     grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
         this.files[0].src.forEach(function(file) {
@@ -149,4 +161,24 @@ module.exports = function(grunt) {
         copyFile.version = packageFile.version;
         grunt.file.write(configFileName , JSON.stringify(copyFile));
     });
+
+    function decideDist()
+    {
+        if(process.env.buildFor == 'deploy')
+        {
+            grunt.log.writeln("dist is github folder");
+
+            return {
+                root: 'app/dist/'
+            }
+        }
+        else
+        {
+            grunt.log.writeln("dist is local");
+
+            return {
+                root: '../../../dist/content/viewers/atomic/v1/js/'
+            }
+        }
+    }
 };
--- dist/sarine.viewer.rough.bundle.js
+++ dist/sarine.viewer.rough.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.rough - v1.0.0 -  Monday, January 1st, 2018, 2:27:23 PM 
+sarine.viewer.rough - v1.0.0 -  Tuesday, January 2nd, 2018, 5:07:30 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
