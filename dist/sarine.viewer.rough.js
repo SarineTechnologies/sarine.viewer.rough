46fa782e4091439f49f9787f43bf2c42e6df8370
ronishoham <roni.shoham@sarine.com>
2018-01-25 16:41:02 +0200

    done task 107957

--- dist/sarine.viewer.rough.js
+++ dist/sarine.viewer.rough.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.rough - v1.0.0 -  Tuesday, January 2nd, 2018, 5:07:30 PM 
+sarine.viewer.rough - v1.0.0 -  Thursday, January 25th, 2018, 4:40:41 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
